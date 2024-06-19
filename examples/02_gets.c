28e5a40d47d50e7816073a61a541ff61537b9c86
Sahil Sihag <29810589+sahilsihag@users.noreply.github.com>
2024-06-19 14:41:47 +0200

    remove old compiler warning

--- examples/02_gets.c
+++ examples/02_gets.c
@@ -1,15 +1,5 @@
 // --gets option usecase example
 
-// Compiler warns here:
-// $ gcc 02_gets.c -o 02_gets                                                                                                                                                                9:24:27
-// 02_gets.c: In function ‘main’:
-// 02_gets.c:11:5: warning: implicit declaration of function ‘gets’; did you mean ‘fgets’? [-Wimplicit-function-declaration]
-//    11 |     gets(string);
-// |     ^~~~
-// |     fgets
-// /usr/bin/ld: /tmp/ccHdyF2w.o: in function `main':
-// 02_gets.c:(.text+0x46): warning: the `gets' function is dangerous and should not be used.
-
 // https://cplusplus.com/reference/cstdio/gets/
 // [NOTE: This function is no longer available in C or C++ (as of C11 & C++14)]
 
