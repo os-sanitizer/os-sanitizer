42629b961bad7d2645a53765826b048632a0d19e
Sahil Sihag <29810589+sahilsihag@users.noreply.github.com>
2024-07-02 16:18:59 +0200

    add benign line to printf_mutability example

--- examples/08_printf_mutability.c
+++ examples/08_printf_mutability.c
@@ -13,6 +13,7 @@ int main (int argc, char **argv)
     MICROBENCHMARK_LOOP_START
 
     printf(argv[0]);
+    printf("Benign print\n");
     debug_printf("Success.\n");
 
     MICROBENCHMARK_LOOP_END
