d193ba1ebf9c0553a099314d3b3e666f395c7eac
Sahil Sihag <29810589+sahilsihag@users.noreply.github.com>
2024-07-08 09:53:44 +0200

    improve 11_filep_unlocked performance: create threads once and goto infinity loop

--- examples/common.c
+++ examples/common.c
@@ -2,10 +2,27 @@
 #include <stdio.h>
 #include "common.h"
 
+#ifdef MICROBENCHMARK_FP_UNL
+extern long interation_count_t1;
+extern long interation_count_t2;
+extern long interation_count_t3;
+extern long interation_count_t4;
+extern FILE* fp;
+#else
 extern long interation_count;
+#endif
 
+#ifdef MICROBENCHMARK_FP_UNL
+void sigint_handler()
+{
+    printf("Iterations: %ld\n", interation_count_t1 + interation_count_t2 + interation_count_t3 + interation_count_t4);
+    fclose(fp);
+    exit(0);
+}
+#else
 void sigint_handler()
 {
     printf("Iterations: %ld\n", interation_count);
     exit(0);
 }
+#endif
